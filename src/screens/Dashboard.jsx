638bbc893fd82bf2715a63cd9bad6a3982e03957
Rajshree Jajoo <rajshreejajoo77@gmail.com>
2024-07-19 02:35:22 +0530

    remove commented code

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -3,25 +3,6 @@ import TodoCard from './TodoCard';
 const Dashboard = ({searchVal,todoList,setTodoList}) => {
 
   return (
-  //   <div className="grid-container">  
-  //   {(searchVal === "" || searchVal === 'All') ?
-  //   todoList?.map((item,index) => (
-  //     <TodoCard status={item.status} id={item.id} title={item.title} description={item.description} key={index} todoList={todoList} setTodoList={setTodoList}/>
-  //     ))
-  //   : 
-  //   todoList?.map((item,index) => {
-  //   if(item.status === searchVal)
-  //   {
-  //     return (
-  //       <TodoCard status={item.status} title={item.title} description={item.description} key={index} todoList={todoList} setTodoList={setTodoList}/>
-  //     )
-    
-  //   }  
-  //   })
-  //   } 
-  // </div> 
-  
-
     <div className="grid-container">  
     {(searchVal === "" || searchVal === 'All') ?
     todoList?.map((item,index) => (
@@ -34,12 +15,14 @@ const Dashboard = ({searchVal,todoList,setTodoList}) => {
       return (
         <TodoCard status={item.status} title={item.title} description={item.description} key={index} todoList={todoList} setTodoList={setTodoList}/>
       )
+    
     }  
-    }
-  
-    )
+    })
     } 
   </div> 
+  
+
+    
   )
 }
 
